0b439e7b309f3b65b14378afd62b97a73964f449
Wiliam Souza <wiliamsouza83@gmail.com>
2014-05-21 22:12:47 -0300

    Fix install file

--- install.sh
+++ install.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-DEVEL_DIR=$HOME/devel
-SOURCE_DIR=$HOME/source
-DOT_REPO_DIR=$SOURCE_DIR
 LOCAL_BIN=$HOME/.local/bin
 
-mkdir -p $DEVEL_DIR
-mkdir -p $SOURCE_DIR
 mkdir -p $LOCAL_BIN
 
-sudo apt-get install openssh-server git gnome-tweak-tool vim tmux screen -y
+cat .bashrc >> $HOME/.bashrc
+cp .inputrc .pythonrc.py .tmux.conf .vimrc .screenrc $HOME
+cp tmuxstart $LOCAL_BIN
 
-git clone https://github.com/wiliamsouza/dot.git $DOT_REPO_DIR
-pushd $DOT_REPO_DIR
-bash install.sh
-popd
