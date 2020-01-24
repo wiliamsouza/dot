e4dadead2ca8da02e2cdeb317314373b39d63650
Wiliam Souza <wiliamsouza83@gmail.com>
2020-01-24 12:35:02 -0300

    Install neovim config

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ mkdir -p $LOCAL_BIN
 
 cat .bashrc >> $HOME/.bashrc
 cp .gitignore_global .inputrc .pythonrc.py .tmux.conf .screenrc $HOME
+mkdir -p $HOME/.config/nvim/init.vim
+cp init.vim $HOME/.config/nvim/
 cp tmuxstart $LOCAL_BIN
 chmod +x $LOCAL_BIN/tmuxstart
