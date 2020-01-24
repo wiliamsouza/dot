a290ffdc1787d0ff74b0f2e524d3e5d4e76706b3
Wiliam Souza <wiliamsouza83@gmail.com>
2020-01-24 12:34:20 -0300

    Change to not install vimrc

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ LOCAL_BIN=$HOME/.local/bin
 mkdir -p $LOCAL_BIN
 
 cat .bashrc >> $HOME/.bashrc
-cp .gitignore_global .inputrc .pythonrc.py .tmux.conf .vimrc .screenrc $HOME
+cp .gitignore_global .inputrc .pythonrc.py .tmux.conf .screenrc $HOME
 cp tmuxstart $LOCAL_BIN
 chmod +x $LOCAL_BIN/tmuxstart
