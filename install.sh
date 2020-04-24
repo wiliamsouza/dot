6e8946e53ed30b5a1f0f1d8dcf03be62c5621797
Wiliam Souza <wiliamsouza83@gmail.com>
2020-04-23 22:23:41 -0300

    Fix nvim config instalation

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p $LOCAL_BIN
 
 cat .bashrc >> $HOME/.bashrc
 cp .gitignore_global .inputrc .pythonrc.py .tmux.conf .screenrc $HOME
-mkdir -p $HOME/.config/nvim/init.vim
+mkdir -p $HOME/.config/nvim/
 cp init.vim $HOME/.config/nvim/
 cp tmuxstart $LOCAL_BIN
 chmod +x $LOCAL_BIN/tmuxstart
